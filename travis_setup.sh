d58bdc1b8bb0a8209bc108c8848fb4bd2eef7cb3
Julian Zucker <julian.zucker@gmail.com>
2019-05-19 23:43:53 -0400

    Try /usr/local/bin
    Authored-by: Julian Zucker <julian.zucker@gmail.com>

--- travis_setup.sh
+++ travis_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget -O /usr/bin/scalastyle_2.12-1.0.0-batch.jar https://oss.sonatype.org/content/repositories/releases/org/scalastyle/scalastyle_2.12/1.0.0/scalastyle_2.12-1.0.0-batch.jar
+wget -O /usr/local/bin/scalastyle_2.12-1.0.0-batch.jar https://oss.sonatype.org/content/repositories/releases/org/scalastyle/scalastyle_2.12/1.0.0/scalastyle_2.12-1.0.0-batch.jar
 echo "#!/bin/bash\n\
-java -jar /usr/bin/scalastyle_2.12-1.0.0-batch.jar \"\$\@\"" > /usr/bin/scalastyle
-chmod +x /usr/bin/scalastyle
+java -jar /usr/local/bin/scalastyle_2.12-1.0.0-batch.jar \"\$\@\"" > /usr/local/bin/scalastyle
+chmod +x /usr/local/bin/scalastyle
