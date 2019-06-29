74590b7fcf5ffcd1e365b73e7834a985bdb53a20
Julian Zucker <julian.zucker@gmail.com>
2019-06-29 10:35:00 -0400

    Add convenience methods to Scored/ParetoFrontier
    * dominates, dominatedBy, etc will be helpful for the
    ImmigrationStrategies
    
    Authored-by: Julian Zucker <julian.zucker@gmail.com>

--- src/main/scala/com/evvo/island/population/paretofrontier.scala
+++ src/main/scala/com/evvo/island/population/paretofrontier.scala
@@ -14,12 +14,21 @@ case class ParetoFrontier[Sol](solutions: Set[Scored[Sol]]) {
        """.stripMargin)
   }
 
-
   override def toString: String = {
     val contents = solutions.map(_.score.toVector.map {
       case ((name, dir), score) => name -> score
     }.toMap).mkString("\n  ")
     f"ParetoFrontier(\n  ${contents})"
+  }
+
+  /** @return Whether this Pareto frontier contains a point that dominates the given solution. */
+  def dominates(sol: Scored[Sol]): Boolean = {
+    this.solutions.exists(_.dominates(sol))
+  }
+
+  /** @return Whether this Pareto frontier is domianted by the given solution. */
+  def dominatedBy(sol: Scored[Sol]): Boolean = {
+    this.solutions.exists(sol.dominates)
   }
 }
 
@@ -31,32 +40,7 @@ object ParetoFrontier {
   // TODO test this for performance, and optimize - this is likely to become a bottleneck
   // https://static.aminer.org/pdf/PDF/000/211/201/on_the_computational_complexity_of_finding_the_maxima_of_a.pdf
   def setToParetoFrontier[Sol](solutions: Set[Scored[Sol]]): Set[Scored[Sol]] = {
-
-    // mutable set used here for performance, converted back to immutable afterwards
-    val out: mutable.Set[Scored[Sol]] = mutable.Set()
-
-    for (sol <- solutions) {
-      // if, for all other elements in the population..
-      if (solutions.forall(other => {
-        // (this part just ignores the solution that we are looking at)
-        sol == other ||
-          // there is at least one score that this solution beats other solutions at,
-          // (then, that is the dimension along which this solution is non dominated)
-          sol.score.zip(other.score).exists({
-            case (((name1, direction), score1), ((name2, _), score2)) => direction match {
-              case Minimize => score1 < score2
-              case Maximize => score1 > score2
-            }
-          }) ||
-          sol.score.zip(other.score).forall({
-            case ((name1, score1), (name2, score2)) => score1 == score2
-          })
-      })) {
-        // then, we have found a non-dominated solution, so add it to the output.
-        out.add(sol)
-      }
-    }
-    out.toSet
+    solutions.filterNot(s => solutions.exists(_.dominates(s)))
   }
 
   def isParetoFrontier[Sol](solutions: Set[Scored[Sol]]): Boolean = {
