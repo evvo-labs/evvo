f195825d62a098e8ede31b306e3d7ccb45456101
Daniel Rassaby <drassaby@gmail.com>
2020-02-23 15:59:54 -0500

    Redis migrators use islandIds instead of queue IDs

--- src/main/scala/io/evvo/migration/redis.scala
+++ src/main/scala/io/evvo/migration/redis.scala
@@ -1,10 +1,9 @@
 package io.evvo.migration
 
-import java.util.UUID
-
 import com.redis.RedisClient
 import com.redis.serialization.Parse
 import io.evvo.island.population.{Maximize, Minimize, ParetoFrontier, Scored}
+import io.evvo.island.utilities.{log, islandId}
 import org.json4s._
 import org.json4s.native.Serialization
 object redis {
@@ -22,6 +21,9 @@ object redis {
           Serialization.formats(FullTypeHints(List(classOf[Minimize], classOf[Maximize])))
 
         val maybeTarget = redisClient.srandmember[String](emigrationTargetsKey)
+        log.debug(
+          s"Emigrating the following solutions from $islandId to $emigrationTargetsKey:\n" +
+            s"$solutions")
         maybeTarget.foreach(target => {
           val data = solutions.map(Serialization.write[Scored[Sol]])
           if (data.length == 1) {
@@ -39,20 +41,21 @@ object redis {
     * islands are using a RedisEmigrator.
     */
   class RedisImmigrator[Sol: Manifest](redisClient: RedisClient) extends Immigrator[Sol] {
-    val uuid: UUID = UUID.randomUUID()
 
     /** What is the list value that this immigration UUID should pop from? */
-    val queueKey: String = f"${ns}::immigration_queues::${uuid.toString}"
+    val queueKey: String = f"${ns}::immigration_queues::${islandId}"
     redisClient.sadd(emigrationTargetsKey, queueKey)
 
     override def immigrate(numberOfImmigrants: Int): Seq[Scored[Sol]] = {
+      log.debug(s"Immigrating $numberOfImmigrants from $emigrationTargetsKey $queueKey")
+
       implicit val formats: Formats =
         Serialization.formats(FullTypeHints(List(classOf[Minimize], classOf[Maximize])))
       val result = redisClient.pipeline(redisClient => {
         redisClient.lrange[String](this.queueKey, 0, numberOfImmigrants)
         redisClient.ltrim(this.queueKey, numberOfImmigrants, -1)
       })
-      result.fold(Seq[Scored[Sol]]())(
+      val toJoinIsland: Seq[Scored[Sol]] = result.fold(Seq[Scored[Sol]]())(
         _.asInstanceOf[List[Option[List[Option[String]]]]].head
           .fold(Seq[Scored[Sol]]())(lrangeResult => {
             lrangeResult.collect {
@@ -60,6 +63,8 @@ object redis {
                 Serialization.read[Scored[Sol]](str)
             }
           }))
+      log.debug(s"Immigrating the following solutions:\n$toJoinIsland")
+      toJoinIsland
     }
   }
 
